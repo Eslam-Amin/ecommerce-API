87d6c75697f17093706e942baaf4e31375875ba5
Eslam Amin <eslam.amin.610@gmail.com>
2026-05-02 17:15:25 +0300

    feat(repository): create store with a repository

---
+++ service/store.go
@@ -0,0 +1,57 @@
+package user
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/Eslam-Amin/ecommerce/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStroe(db *sql.DB) *Store {
+
+	return &Store{db}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users where email=?", email)
+	if err != nil {
+		return nil, err
+	}
+
+	user := new(types.User)
+	for rows.Next() {
+		user, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if user.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return user, nil
+}
+
+func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
+	user := new(types.User)
+
+	err := rows.Scan(
+		&user.ID,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
