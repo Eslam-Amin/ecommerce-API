859067a4d8e87f6df9ecee3e0481a46be7d6e8fc
Eslam Amin <eslam.amin.610@gmail.com>
2026-05-03 02:52:21 +0300

    fix: add the right word for the PORT

--- config/env.go
+++ config/env.go
@@ -26,7 +26,7 @@ func initConfig() Config {
 	}
 	return Config{
 		PublicHost: getEnv("PUBLIC_HOST", "http://localhost"),
-		Port:       getEnv("POST", "8080"),
+		Port:       getEnv("PORT", "8080"),
 		DBUser:     getEnv("DB_USER", "root"),
 		DBPassword: getEnv("DB_Password", "random_password"),
 		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
