103ef82e1b4120d369e1cd6f520016ca9ec5ff3c
Eslam Amin <eslam.amin.610@gmail.com>
2026-05-03 03:31:55 +0300

    refactor: add any to data in reponse body struct

--- types/types.go
+++ types/types.go
@@ -28,8 +28,8 @@ type LoginUserPayload struct {
 }
 
 type ResponseBody struct {
-	Success bool           `json:"success"`
-	Message string         `json:"message,omitempty"`
-	Error   string         `json:"error,omitempty"`
-	Data    map[string]any `json:"data,omitempty"`
+	Success bool   `json:"success"`
+	Message string `json:"message,omitempty"`
+	Error   string `json:"error,omitempty"`
+	Data    any    `json:"data,omitempty"`
 }
