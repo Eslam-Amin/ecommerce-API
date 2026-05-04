fc20eaec7896e8ff4d0d0614b19ba6dc65a0dc3c
Eslam Amin <eslam.amin.610@gmail.com>
2026-05-05 01:28:07 +0300

    feat: register cart routes in the main api file

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Eslam-Amin/ecommerce/service/cart"
+	"github.com/Eslam-Amin/ecommerce/service/order"
 	"github.com/Eslam-Amin/ecommerce/service/product"
 	"github.com/Eslam-Amin/ecommerce/service/user"
 	"github.com/gorilla/mux"
@@ -33,6 +35,10 @@ func (s *APIServer) Run() error {
 	productStore := product.NewStore(s.db)
 	productHandler := product.NewHandler(productStore)
 	productHandler.RegisterRoutes(subrouter)
+
+	orderStore := order.NewStore(s.db)
+	cartHandler := cart.NewHandler(orderStore, productStore)
+	cartHandler.RegisterRoutes(subrouter)
 
 	log.Println("Listening on: ", s.addr)
 	return http.ListenAndServe(s.addr, router)
